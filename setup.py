c63d8381fd308d1dec6cab88f88c61ba0b230a9f
Jan Clemens <clemensjan@googlemail.com>
2020-06-16 11:48:50 +0200

    add flammkuchen dep, version bump

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import setup
 
 setup(
     name='attrdict',
-    version='0.1',
+    version='0.2',
     author='Jan Clemens',
     py_modules=['attrdict'],
     python_requires='>=3.5',
     keywords='attrs dict',
+    install_requires=['flammkuchen'],
 )
